f25b38365e76614b48f3e812de9a13915afeb319
jwang1 <sigkdd@yahoo.com>
2018-04-28 23:55:04 -0400

    std::cin.ignore()
    wait for input, but if it happens, just ignore the input.

--- main.cpp
+++ main.cpp
@@ -4,5 +4,10 @@ using namespace std;
 
 int main() {
     cout << "Hello, Tic Tac Toe!" << endl;
+    // waiting for input, but if user inputs, just ignores it, and move the the next statement (return 0)
+    cin.ignore();
+
+
+
     return 0;
 }
