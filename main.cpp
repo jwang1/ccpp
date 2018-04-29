c9d94f95810533312baabb9da881be09587dc20e
jwang1 <sigkdd@yahoo.com>
2018-04-29 00:00:31 -0400

    getline and cin

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
     cout << "Hello, Tic Tac Toe!" << endl;
+
     // waiting for input, but if user inputs, just ignores it, and move the the next statement (return 0)
-    cin.ignore();
+    // cin.ignore();
+
+    string name;
+
+    cout << "enter your name: ";
+
+    getline(cin, name);
+
+
+    cout << "You've entered name: " << name << endl;
+
+
+    // cin.ignore();
 
 
 
