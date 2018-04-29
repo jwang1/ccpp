ec27e98b583ba6abb25b90ea69311c0cf74e175e
jwang1 <sigkdd@yahoo.com>
2018-04-29 15:53:39 -0400

    Adding Header files for the function prototypes.

---
+++ util.h
@@ -0,0 +1,23 @@
+//
+// Created by JWang on 4/29/18.
+//
+#include <string>
+
+using namespace std;
+
+#ifndef TICTACTOE_UTIL_H
+#define TICTACTOE_UTIL_H
+
+
+void checkText();
+
+void print(string);
+
+
+void checkIntegerDouble();
+
+void checkCharWchar();
+
+void checkArray();
+
+#endif //TICTACTOE_UTIL_H
