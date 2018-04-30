944298a371d95c4ecdf5514b6586625d6299ddfe
jwang1 <sigkdd@yahoo.com>
2018-04-29 22:18:08 -0400

    deep dive into c-tor, d-tor, and static memory allocation, dynamic memory allocation

--- classes.cpp
+++ classes.cpp
@@ -8,11 +8,29 @@
 void classDrive() {
 
     {
+        cout << "START:__Dynamic default-ctor" << endl;
+
         // check destructor
         // ts is out of scope
         Cat *tc = new Cat();        // Note, Cat tc();   NOT working,  and it cannot do tc.getAge() later;   Why had to use new?   Only new calls default-c-tor
 
         cout << "checking empty object data member : " << tc->getAge() << endl;
+
+        // dynamic allocation, need specific de-allocate memory
+        delete tc;
+
+        cout << "END:__Dynamic" << endl << endl;
+    }
+
+    {
+        cout << "BEGIN:____static way default-ctor calling..." << endl;
+
+        Cat tc;  // again, try the above, static way constructing,  it should call default-c-tor
+
+        cout << "static default ctor obj: " << tc.getName() << endl;
+
+        cout << "END:___static" << endl << endl;
+
     }
 
     Cat cat("tiger", 6);
@@ -21,5 +39,7 @@ void classDrive() {
          << endl;
 
     cat.talk();
+
+    cout << "exits classDrive()" << endl << endl;
 
 }
